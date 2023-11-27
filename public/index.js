73b7ae142d643363271711b4475b9cf4afa63cfe
Joe Watt <joewatt95@u.nus.edu>
2023-11-27 16:14:51 +0800

    cleanup imports

--- public/index.js
+++ public/index.js
@@ -1,13 +1,9 @@
 import {
-    query_le,
-    query_le_js,
-    re_init_le_swipl,
-    query_le_wasm,
-    query_le_wasm_js,
-    render_le_resp_with_guifier,
-    clj_to_js,
-    js_to_clj
-  } from "./js/main.js";
+  query_le_js,
+  re_init_le_swipl,
+  query_le_wasm_js,
+  render_le_resp_with_guifier
+} from "./js/main.js";
 
 const program = await fetch("program.le").then(x => x.text());
 const data = await fetch("data.json").then(x => x.json());
@@ -21,7 +17,7 @@ const resp = await query_le_js(server_url, program, data, query);
 
 // Query alternative wasm backend.
 // await re_init_le_swipl("le.qlf");
-// const resp = await query_le_wasm(program, data, query);
+// const resp = await query_le_wasm_js(program, data, query);
 
 console.log("LE explanation tree: ", resp);
 

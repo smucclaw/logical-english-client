f6e2836540842e76f423152f60e386e47749557b
Joe Watt <joewatt95@u.nus.edu>
2023-11-27 22:11:04 +0800

    cleanup

--- src/logical_english_client/webform_facts_to_le/internal/core.cljs
+++ src/logical_english_client/webform_facts_to_le/internal/core.cljs
@@ -21,51 +21,9 @@
 (defn- asami-triples->le-datoms [asami-triples]
   (eduction (map asami-triple->le-datom) asami-triples))
 
-#_(defn- f [le-scenario data n]
-  (let [dob
-        (m/find data
-                (m/$ {:dob (m/some ?date)})
-                (date/str->maybe-date ?date))
-
-        nth-birth-date
-        (tick/>> dob (tick/new-period n :years))
-
-        policy-start-date
-        (m/find data
-                (m/$ {:risk_commencement_date (m/some ?date)})
-                (date/str->maybe-date ?date))
-
-        num-years (tick/between policy-start-date nth-birth-date :years)
-
-        last-policy-anniversary
-        (tick/>> policy-start-date (tick/new-period num-years :years))
-
-        date
-        (m/find data
-                (m/or (m/$ {:date_of_accident (m/some ?date)})
-                      (m/$ {:date_of_diagnosis (m/some ?date)}))
-                (date/str->maybe-date ?date))
-        
-        in-same-year
-        (= 0 (tick/between last-policy-anniversary date :years))]
-
-    ;; (js/console.log "Stuff: " in-same-year)
-    (if in-same-year
-      (str le-scenario
-           "\n"
-           "a life assured turns " n " in one policy year of the accident or diagnosis.")
-      le-scenario)))
-
 (malli/=> clj-data->le-scenario [:=> [:cat :any] :string])
 (defn clj-data->le-scenario [clj-data]
   (-> clj-data
       asami/clj-data->triples
       asami-triples->le-datoms
-      datom/le-datoms->le-scenario
-
-      ;; Generate the following if applicable:
-      ;; "a life assured turns 65 in one policy year of the accident or diagnosis.")
-      ;; "a life assured turns 75 in one policy year of the accident or diagnosis.")
-      ;; (f clj-data 65)
-      ;; (f clj-data 75)
-      ))
+      datom/le-datoms->le-scenario))

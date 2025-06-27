78a74dcbf4806c362307e8cfb52117c11c28340a
Joe Watt <joewatt95@u.nus.edu>
2025-06-27 09:43:52 +0800

    bump

--- src/logical_english_client/le_api_client/swipl_wasm/core.cljs
+++ src/logical_english_client/le_api_client/swipl_wasm/core.cljs
@@ -5,7 +5,7 @@
             [shadow.esm :refer [dynamic-import]]))
 
 (def ^:private swipl-wasm-cdn-url
-  "https://SWI-Prolog.github.io/npm-swipl-wasm/5/0/11/dynamic-import.js")
+  "https://SWI-Prolog.github.io/npm-swipl-wasm/5/0/12/dynamic-import.js")
 
 (def ^:private le-swipl
   (atom nil))
